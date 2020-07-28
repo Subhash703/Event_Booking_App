56c9cc0876d53ea9785786df15f3f21661eb5971
Subhash Chandra <49841848+Subhash703@users.noreply.github.com>
2020-07-28 10:20:25 +0530

    Added javaScript to the app

---
+++ app.js
@@ -0,0 +1,17 @@
+const home = document.getElementById('home');
+
+const images = [
+    "https://images.unsplash.com/photo-1517048676732-d65bc937f952?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" ,
+    "https://images.unsplash.com/photo-1558403194-611308249627?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" ,
+    "https://images.unsplash.com/photo-1573164574572-cb89e39749b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    "https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    "https://images.unsplash.com/photo-1517048676732-d65bc937f952?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+]
+const changeBakground = () => {
+    var randomIndex = Math.floor(Math.random()*6);
+    home.style.backgroundImage=`url(${images[randomIndex]})`;
+    console.log(images[randomIndex]);
+}
+
+setInterval(changeBakground(),5000);
